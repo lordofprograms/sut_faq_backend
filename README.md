05852e1b0ad4c2e6a5961f5016b1f4cfc0fe64e4
lordofprograms <shelegmike@gmail.com>
2021-05-03 15:29:08 +0300

    unignore env file for example and add readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# SUT FAQ backend service
+
+Stack:
+- python3
+- fastAPI
+- huggingface
+- elasticsearch
+- numpy
+- pandas
+
+## If you are on Linux server: install Docker and docker-compose
+`chmod +x install_modules.sh`
+
+`./install_modules.sh`
+
+## Then run project
+`docker-compose up`
+
+## Stop project
+`docker-compose stop`
---
+++ install_modules.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y curl
+sudo apt-get install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
